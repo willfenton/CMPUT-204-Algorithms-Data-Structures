e64bcaf911839c6130b1d511144e3dc81e38c4df
Will Fenton <wfenton@ualberta.ca>
2019-01-11 09:07:00 -0700

    Non-increasing insertion sort

--- insertionSort.py
+++ insertionSort.py
@@ -24,6 +24,16 @@ def insertionSort(array):
         array[i + 1] = key
     return array
 
+def insertionSortNonIncreasing(array):
+    for j in range(1, len(array)):
+        key = array[j]
+        i = j - 1
+        while i >= 0 and key > array[i]:
+            array[i + 1] = array[i]
+            i -= 1
+        array[i + 1] = key
+    return array
+
 # Ensures that the algorithm sorts correctly
 def testCorrectness():
     tests = 10
